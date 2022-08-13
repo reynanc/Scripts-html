1fe97d8df384780ec6ae0f5a171d620fd3855ae3
José Reynan <101825818+reynanc@users.noreply.github.com>
2022-08-13 14:44:39 -0300

    Update script-iac2.sh

--- script-iac2.sh
+++ script-iac2.sh
@@ -12,5 +12,5 @@ cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 unzip main.zip
 cd linux-site-dio-main
-cp -p * /var/www/html/
+cp -r * /var/www/html/
 
